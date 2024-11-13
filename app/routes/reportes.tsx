16a215e7b0a43ad69d5c097bf3c16e42d77af2a7
Daniel Payan <dany.sasket@gmail.com>
2024-11-12 22:47:15 -0600

    ✨🚧feat-add-prisma-stuff

--- app/routes/reportes.tsx
+++ app/routes/reportes.tsx
@@ -2,6 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 import { Button } from "../components/button";
 import LinkTo from "~/components/link";
 import { Table } from "~/components/table";
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,10 +12,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+
+    const client = new PrismaClient();
+    const dataForm = await client.lastData.findMany();
+
+    console.log(dataForm);
+    return {
+        dataForm
+    }
+}
+
+
+
 export default function Reportes() {
+
+    const {dataForm} = useLoaderData<typeof loader>()
+
   return (
     <>
-      <Table title="Reporte general" subtitle="Reporte sobre todos los vehiculos agregados"/>
+
+        <Table title="Reporte general" subtitle="Reporte sobre todos los vehiculos agregados"/>
     </>
   );
 }
